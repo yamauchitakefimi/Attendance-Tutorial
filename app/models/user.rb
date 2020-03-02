2e7a8f393fcc80ff728abe70dd0bca03ebf5dac9
yamauchi <y-trs-y@au.com>
2020-03-02 12:30:03 +0000

    Finish japanization-and-time-format

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true    
+                    uniqueness: true
+  validates :department, length: { in: 2..50 }, allow_blank: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
@@ -36,6 +37,7 @@ class User < ApplicationRecord
 
   # トークンがダイジェストと一致すればtrueを返します。
   def authenticated?(remember_token)
+    # ダイジェストが存在しない場合はfalseを返して終了します。
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
